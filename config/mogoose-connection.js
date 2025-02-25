26a80d5c2de23cb4a1c7387ad50597a95cd9b568
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-02-25 19:29:41 +0530

    db connected and bit error will be fixed

--- config/mogoose-connection.js
+++ config/mogoose-connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');   
 
 mongoose
-.connect('mongdb://127.0.0.1:27017/shop')
+.connect('mongodb://127.0.0.1:27017/shop')
 .then(() =>{
     console.log('Connected to database');
 })
