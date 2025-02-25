5082933b5fe0284b1ed6c634b9ca425c11995f4a
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-02-25 19:27:21 +0530

    db connectino will be export

--- config/mogoose-connection.js
+++ config/mogoose-connection.js
@@ -2,3 +2,11 @@ const mongoose = require('mongoose');
 
 mongoose
 .connect('mongdb://127.0.0.1:27017/shop')
+.then(() =>{
+    console.log('Connected to database');
+})
+.catch((err) =>{
+    console.log('Error connecting to database', err);
+})
+
+module.exports = mongoose.connection;
