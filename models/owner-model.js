9c1a10fa32c56eaa0d6ce809142b20537546d5bd
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-02-25 19:14:21 +0530

    owner model will be created

---
+++ models/owner-model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const ownerSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    isadmin: Boolean,
+    products: {
+        type: Array,
+        default: []
+    },
+    picture: String,
+    gstno: String
+})
+
+module.exports = mongoose.model('owner', ownerSchema);
