073ec58dd30e8daff395fc501cb74e79bcd82d55
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-02-25 19:34:57 +0530

    Router file will be created

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 
 const db = require('./config/mogoose-connection');
+const ownerModel = require('./models/owner-model');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
+
+app.use('/owners', ownersRouter);
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
 
 app.get('/', (req, res) =>{
