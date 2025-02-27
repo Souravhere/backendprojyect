b961726e5e3a2c4ddb5c7bb54b38a99a5a23531b
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-02-27 19:27:58 +0530

    bit changes config will be added and development.json will be created

--- config/mogoose-connection.js
+++ config/mogoose-connection.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 // here we will setup the debug env 
 // to deactive this >> export DEBUG= press enter  
 const dbgr = require('debug')('development:mongoose'); 
+const config = require('config');
 
 mongoose
-.connect('mongodb://127.0.0.1:27017/shop')
+.connect('/shop')
 .then(() =>{
     dbgr('Connected to database');
 })
