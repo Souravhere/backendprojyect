786d936a1c9053b96c3706954beda8fb6f9869ab
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-02-27 19:31:53 +0530

    dev env will be added

--- config/mogoose-connection.js
+++ config/mogoose-connection.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');  
+const config = require('config');
 // here we will setup the debug env 
 // to deactive this >> export DEBUG= press enter  
 const dbgr = require('debug')('development:mongoose'); 
-const config = require('config');
 
 mongoose
-.connect('/shop')
+.connect(`${config.get('MONGODB_URI')}/shop  `)
 .then(() =>{
     dbgr('Connected to database');
 })
