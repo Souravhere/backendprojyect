232894649e8cfa1843f31244742689b19bd1159e
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-02-27 19:23:55 +0530

    debug env will be setup

--- config/mogoose-connection.js
+++ config/mogoose-connection.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose');   
+const mongoose = require('mongoose');  
+// here we will setup the debug env 
+// to deactive this >> export DEBUG= press enter  
+const dbgr = require('debug')('development:mongoose'); 
 
 mongoose
 .connect('mongodb://127.0.0.1:27017/shop')
 .then(() =>{
-    console.log('Connected to database');
+    dbgr('Connected to database');
 })
 .catch((err) =>{
-    console.log('Error connecting to database', err);
+    dbgr('Error connecting to database', err);
 })
 
 module.exports = mongoose.connection;
