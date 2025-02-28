c02d8023c1ed61fcf52d5c92cbc79baf979ed8d2
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-02-28 18:40:39 +0530

    owner route will be tested

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require('../models/owner-model')
+ 
 
-router.get('/',(req, res) => {
-    res.send('Hello World');
+router.get('/', (req, res) =>{
+    res.send('Welcome to Owner Route')
 })
+if(process.env.NODE_ENV === 'development'){ 
+    router.get('/create',(req, res) => {
+        res.send('Owner Created');
+    })
+}
 
 module.exports = router; 
