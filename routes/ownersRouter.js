3a2547542174c214aba1139e362b16943331621d
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-02-28 18:53:38 +0530

    bit safity check will be added

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -2,14 +2,19 @@ const express = require('express');
 const router = express.Router();
 const ownerModel = require('../models/owner-model')
  
-
+// this is the env for development
+if(process.env.NODE_ENV === 'development'){ 
+    router.post('/create',async (req, res) => {
+        const owner = await ownerModel.create()
+        if(owner.lenth > 0){
+            return res
+            .send(503)
+            .send("You don't have premission to create a new Owner")
+        }
+    })
+}
 router.get('/', (req, res) =>{
     res.send('Welcome to Owner Route')
 })
-if(process.env.NODE_ENV === 'development'){ 
-    router.post('/create',(req, res) => {
-        res.send('Owner Created');
-    })
-}
 
 module.exports = router; 
